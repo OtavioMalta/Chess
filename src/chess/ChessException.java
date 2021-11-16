83f2a6c543d7f760e438d2b3cd9cde80b3afbe9a
OtavioMalta <83037014+OtavioMalta@users.noreply.github.com>
2021-11-16 12:34:50 -0300

    V1

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+import boardgame.BoardException;
+
+public class ChessException extends BoardException{
+	public static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg){
+		super(msg);
+	}
+}
