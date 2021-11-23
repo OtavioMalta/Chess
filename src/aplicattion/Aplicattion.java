4407b6d463fe8048847ce3dce5242d52def43a9e
OtavioMalta <83037014+OtavioMalta@users.noreply.github.com>
2021-11-23 14:43:48 -0300

    Final

--- src/aplicattion/Aplicattion.java
+++ src/aplicattion/Aplicattion.java
@@ -1,6 +1,8 @@
 package aplicattion;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -13,10 +15,13 @@ public class Aplicattion {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		while(true) {
+		
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -31,14 +36,29 @@ public class Aplicattion {
 				ChessPosition target = UI.readChessPosition(sc);
 			
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			}catch(ChessException e){
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q)");
+					String type = sc.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q)");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+					}
+			 }catch(ChessException e){
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}catch(InputMismatchException e){
 				System.out.println(e.getMessage());
-				sc.nextLine();
+				sc.nextLine(); 
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch,captured);
 	}
 
 }
